7146e931169c3e060a5b76d594b7005e36136a37
Abhishek Chapaitkar <abhishek.chapaitkar111@gmail.com>
2023-08-21 16:00:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Learning deployment of react applications.
+          New Changes
         </p>
         <a
           className="App-link"
